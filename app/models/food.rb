da29f3f868b9032650d5b929fa6ace03f2923170
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-04-23 10:23:26 +0800

    Custom form with boostrap but not fix yet

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,26 @@
 class Food < ApplicationRecord
   #for index
   def index
-    
+    @food = Food.all
   end
 
   def create
-    
+    @food = Food.new    
   end
+
+  def edit
+  end
+
+  def create
+    @food = Food.new(menu_params)
+  end
+
+  private
+    def set_food
+      @food = Food.find(params[:id])
+    end
+
+    def menu_params
+      params.require(:food).permit(:name, :price, :category)
+    end
 end
