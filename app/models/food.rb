d45813bec2f9e001f3bf110c676c79f4a09ea970
imadeekosatria <imadeekosatriawiguna@gmail.com>
2022-04-23 16:45:09 +0800

    change languege bahasa to full english

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 150, message: 'Nama maksimum 150 karakter'}
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0.01, message: 'Harga tidak boleh kurang dari 0.01'}
+  validates :name, presence: true, length: { maximum: 150, message: 'maxsimum 150 character'}
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0.01, message: 'can not under 0.02'}
 end
